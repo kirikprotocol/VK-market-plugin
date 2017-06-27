04579f83fc8bb925c7e54a2fa75cb70056de3d8f
Enenko Denis <sony@eyeline.mobi>
2017-06-27 17:12:33 +0700

    gussd_lk-1004: Add blank module for VK Market.

---
+++ src/com/eyelinecom/whoisd/sads2/vk/market/InitListener.java
@@ -0,0 +1,8 @@
+package com.eyelinecom.whoisd.sads2.vk.market;
+
+/**
+ * author: Denis Enenko
+ * date: 27.06.17
+ */
+public class InitListener {
+}
---
+++ src/com/eyelinecom/whoisd/sads2/vk/market/web/servlets/VKMarketServlet.java
@@ -0,0 +1,10 @@
+package com.eyelinecom.whoisd.sads2.vk.market.web.servlets;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ * author: Denis Enenko
+ * date: 27.06.17
+ */
+public class VKMarketServlet extends HttpServlet {
+}
