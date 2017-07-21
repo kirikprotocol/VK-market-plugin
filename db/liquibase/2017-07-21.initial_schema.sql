11eeb169cefac186e39679fd0b19c6a577dd3777
voronov <voronov@eyeline.mobi>
2017-07-21 16:39:29 +0700

    gussd_lk-1027:  - fix liquibase script

--- db/liquibase/2017-07-21.initial_schema.sql
+++ db/liquibase/2017-07-21.initial_schema.sql
@@ -6,60 +6,60 @@
     COLLATE utf8mb4_unicode_ci;
 
   create table cart_items (
-        id integer not null auto_increment,
-        quantity integer not null,
-        vk_item_id integer not null,
-        cart_id integer not null,
-        primary key (id)
+      id integer not null auto_increment,
+      quantity integer not null,
+      vk_item_id integer not null,
+      cart_id integer not null,
+      primary key (id)
     ) ENGINE=InnoDB default CHARSET=utf8;
 
-    create table carts (
-        id integer not null auto_increment,
-        primary key (id)
-    ) ENGINE=InnoDB; default CHARSET=utf8
+  create table carts (
+      id integer not null auto_increment,
+      primary key (id)
+  ) ENGINE=InnoDB default CHARSET=utf8;
 
-    create table order_items (
-        id integer not null auto_increment,
-        quantity integer not null,
-        vk_item_id integer not null,
-        order_id integer not null,
-        primary key (id)
-    ) ENGINE=InnoDB default CHARSET=utf8;
+  create table order_items (
+      id integer not null auto_increment,
+      quantity integer not null,
+      vk_item_id integer not null,
+      order_id integer not null,
+      primary key (id)
+  ) ENGINE=InnoDB default CHARSET=utf8;
 
-    create table orders (
-        id integer not null auto_increment,
-        merchant_email varchar(255) not null,
-        phone_number varchar(255) not null,
-        user_id integer not null,
-        primary key (id)
-    ) ENGINE=InnoDB default CHARSET=utf8;
+  create table orders (
+      id integer not null auto_increment,
+      merchant_email varchar(255) not null,
+      phone_number varchar(255) not null,
+      user_id integer not null,
+      primary key (id)
+  ) ENGINE=InnoDB default CHARSET=utf8;
 
-    create table users (
-        id integer not null auto_increment,
-        user_id varchar(255) not null unique,
-        cart_id integer not null,
-        primary key (id)
-    ) ENGINE=InnoDB default CHARSET=utf8;
+  create table users (
+      id integer not null auto_increment,
+      user_id varchar(255) not null unique,
+      cart_id integer not null,
+      primary key (id)
+  ) ENGINE=InnoDB default CHARSET=utf8;
 
-    alter table cart_items
-        add constraint FK_4p7dd2p61wsdx9j35wp6sugqr
-        foreign key (cart_id)
-        references carts (id);
+  alter table cart_items
+      add constraint FK_4p7dd2p61wsdx9j35wp6sugqr
+      foreign key (cart_id)
+      references carts (id);
 
-    alter table order_items
-        add constraint FK_9gap2fmw66v092ntb58rtohwh
-        foreign key (order_id)
-        references orders (id);
+  alter table order_items
+      add constraint FK_9gap2fmw66v092ntb58rtohwh
+      foreign key (order_id)
+      references orders (id);
 
-    alter table orders
-        add constraint FK_k8kupdtcdpqd57b6j4yq9uvdj
-        foreign key (user_id)
-        references users (id);
+  alter table orders
+      add constraint FK_k8kupdtcdpqd57b6j4yq9uvdj
+      foreign key (user_id)
+      references users (id);
 
-    alter table users
-        add constraint FK_pnp1baae4enifkkuq2cd01r9l
-        foreign key (cart_id)
-        references carts (id);
+  alter table users
+      add constraint FK_pnp1baae4enifkkuq2cd01r9l
+      foreign key (cart_id)
+      references carts (id);
 
 -- rollback ALTER TABLE users DROP FOREIGN KEY FK_pnp1baae4enifkkuq2cd01r9l;
 -- rollback ALTER TABLE orders DROP FOREIGN KEY FK_k8kupdtcdpqd57b6j4yq9uvdj;
