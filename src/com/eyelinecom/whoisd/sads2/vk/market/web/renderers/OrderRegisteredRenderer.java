ca57dbfcf4619e9d5a01c61a733ec345b4c94e38
voronov <voronov@eyeline.mobi>
2017-07-27 17:16:14 +0700

    gussd_lk-1027:  - updated final page at order processing  - new TODOs

--- src/com/eyelinecom/whoisd/sads2/vk/market/web/renderers/OrderRegisteredRenderer.java
+++ src/com/eyelinecom/whoisd/sads2/vk/market/web/renderers/OrderRegisteredRenderer.java
@@ -4,19 +4,17 @@ import com.eyelinecom.whoisd.sads2.vk.market.services.model.OrderDetailed;
 import com.eyelinecom.whoisd.sads2.vk.market.services.model.OrderItemDetailed;
 import com.eyelinecom.whoisd.sads2.vk.market.services.shorturl.UrlResolver;
 import com.eyelinecom.whoisd.sads2.vk.market.web.servlets.RequestParameters;
-import com.eyelinecom.whoisd.sads2.vk.market.web.util.UserInputUtils;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Locale;
-import java.util.ResourceBundle;
+import java.util.*;
 
 /**
  * author: Artem Voronov
  */
 public class OrderRegisteredRenderer extends Renderer {
+
+  private static final int PARTITION_SIZE = 3;
 
   private final ResourceBundle bundle;
   private final OrderDetailed orderDetailed;
@@ -29,28 +27,33 @@ public class OrderRegisteredRenderer extends Renderer {
 
   @Override
   public void render(HttpServletResponse response, String ctxPath, RequestParameters requestParams, UrlResolver urlResolver) throws IOException {
-    String askPhoneNumber = getAskPhoneNumberPage(ctxPath, requestParams, urlResolver);
-
-    sendResponse(response, requestParams, Arrays.asList(askPhoneNumber));
+    sendResponse(response, requestParams, initFeedBackPages(ctxPath, requestParams, urlResolver));
   }
 
-  private String getAskPhoneNumberPage(String ctxPath, RequestParameters requestParams, UrlResolver urlResolver) throws IOException {
-    StringBuilder sb = new StringBuilder();
+  private List<String> initFeedBackPages(String ctxPath, RequestParameters requestParams, UrlResolver urlResolver) throws IOException {
+    List<OrderItemDetailed> items = orderDetailed.getItems();
+    int itemsCount = items.size();
+    if (itemsCount <= PARTITION_SIZE)
+      return Arrays.asList(getOrderRegisteredPage(ctxPath, requestParams, urlResolver));
 
+    List<String> result = new LinkedList<>();
+    result.add(getBasicInfoPage());
+
+    for (int i = 0, partitionCounter = 0; i < itemsCount; i += PARTITION_SIZE, partitionCounter++) {
+      List<OrderItemDetailed> part = items.subList(i, Math.min(i + PARTITION_SIZE, itemsCount));
+      result.add(getOrderListPartitionPage(part, partitionCounter));
+    }
+
+    result.add(getFinalKeyBoardPage(ctxPath, requestParams, urlResolver));
+    return result;
+  }
+
+  private String getBasicInfoPage() throws IOException {
+    StringBuilder sb = new StringBuilder();
     sb.append(pageStart());
     sb.append(divStart());
     sb.append(String.format(bundle.getString("order.is.registered"), orderDetailed.getId()));
     sb.append(br());
-    sb.append(br());
-    sb.append(bundle.getString("order.list")).append(":");
-    sb.append(br());
-    List<OrderItemDetailed> items = orderDetailed.getItems();
-    int count = 0;
-    for (OrderItemDetailed it : items) {//TODO: если список будет большим, использовать пуши или вообще сделать отправку по email
-      sb.append(++count).append(". ").append(it.getName()).append(" x ").append(it.getQuantity()).append(". ")
-        .append(bundle.getString("price.per.unit")).append(" - ").append(it.getPrice());
-      sb.append(br());
-    }
     sb.append(br());
     sb.append(String.format(bundle.getString("total.cost"), orderDetailed.getTotalCost()));
     sb.append(br());
@@ -59,10 +62,69 @@ public class OrderRegisteredRenderer extends Renderer {
     sb.append(br());
     sb.append(String.format(bundle.getString("merchant.email"), orderDetailed.getMerchantEmail()));
     sb.append(divEnd());
+    sb.append(pageEnd());
+    return sb.toString();
+  }
+
+  private String getOrderListPartitionPage(List<OrderItemDetailed> partition, int partitionCounter) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    sb.append(pageStart());
+    sb.append(divStart());
+    orderList(partition, partitionCounter * PARTITION_SIZE, sb);
+    sb.append(divEnd());
+    sb.append(pageEnd());
+    return sb.toString();
+  }
+
+  private String getFinalKeyBoardPage(String ctxPath, RequestParameters requestParams, UrlResolver urlResolver) throws IOException {
+    StringBuilder sb = new StringBuilder();
+    sb.append(pageStart());
+    exitButtons(sb, ctxPath, requestParams, urlResolver);
+    sb.append(pageEnd());
+    return sb.toString();
+  }
+
+  private void exitButtons(StringBuilder sb, String ctxPath, RequestParameters requestParams, UrlResolver urlResolver) throws IOException {
     sb.append(buttonsStart());
     sb.append(button("", bundle.getString("continue.shopping"), requestParams.getPluginParams(), ctxPath, "/", urlResolver));
     sb.append(buttonExit(requestParams.getPluginParams(), ctxPath));
+    //TODO: во время пуша через сценарий xmlpush
+    //Error on line 1 of document : The reference to entity "sver" must end with the ';' delimiter. Nested exception: The reference to entity "sver" must end with the ';' delimiter.
     sb.append(buttonsEnd());
+  }
+
+  private void orderList(List<OrderItemDetailed> items, int startCounterWith, StringBuilder sb) throws IOException {
+    if (startCounterWith == 0) {
+      sb.append(bundle.getString("order.list")).append(":");
+      sb.append(br());
+    }
+    int count = startCounterWith;
+    for (OrderItemDetailed it : items) {
+      sb.append(++count).append(". ").append(it.getName()).append(" x ").append(it.getQuantity()).append(". ")
+        .append(bundle.getString("price.per.unit")).append(" - ").append(it.getPrice());
+      sb.append(br());
+    }
+  }
+
+  private String getOrderRegisteredPage(String ctxPath, RequestParameters requestParams, UrlResolver urlResolver) throws IOException {
+    StringBuilder sb = new StringBuilder();
+
+    sb.append(pageStart());
+    sb.append(divStart());
+    sb.append(String.format(bundle.getString("order.is.registered"), orderDetailed.getId()));
+    sb.append(br());
+    sb.append(br());
+    sb.append(String.format(bundle.getString("total.cost"), orderDetailed.getTotalCost()));
+    sb.append(br());
+    sb.append(br());
+    sb.append(bundle.getString("wait.for.contact"));
+    sb.append(br());
+    sb.append(String.format(bundle.getString("merchant.email"), orderDetailed.getMerchantEmail()));
+    sb.append(br());
+    sb.append(br());
+    orderList(orderDetailed.getItems(), 0, sb);
+    sb.append(divEnd());
+    exitButtons(sb, ctxPath, requestParams, urlResolver);
     sb.append(pageEnd());
 
     return sb.toString();
